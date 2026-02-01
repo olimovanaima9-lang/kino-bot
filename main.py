6d67fe80b439b6b3ee3578d3d4bb724c507b0a06
olimovanaima9-lang <olimovanaima9@gmail.com>
2026-02-01 20:33:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("TOKEN")
+CHANNEL_USERNAME = "@kinouzbek"   # o'z kanaling username
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    member = await context.bot.get_chat_member(CHANNEL_USERNAME, user_id)
+
+    if member.status in ["member", "administrator", "creator"]:
+        await update.message.reply_text("🎬 Kino linki shu yerda")
+    else:
+        await update.message.reply_text(
+            f"❌ Avval kanalga obuna bo‘ling:\nhttps://t.me/{CHANNEL_USERNAME[1:]}\n\nKeyin /start bosing."
+        )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
